d4f450552e9eacebe13f3c43722b74df9e1379a1
gregory malecha <gmalecha@cs.harvard.edu>
2012-09-07 17:32:58 -0400

    fixing setup.

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 if [ -e coq-ext-lib ]
 then
   echo "************************************************************"
-  echo "** you seem to already have coq-ext-lib installed"
-  echo "** going to update"
+  echo "** You seem to already have coq-ext-lib installed"
+  echo "** Going to update"
   echo "************************************************************"
-  (cd coq-ext-lib; git up)
+  (cd coq-ext-lib; git pull -u; make)
 else
   echo "************************************************************"
-  echo "** You don't have coq-ext-lib. I'm going to pull it."
+  echo "** You don't have coq-ext-lib. I'm going to pull a read-only version"
   echo "** If you have it already, you can sym-link it."
   echo "************************************************************"
   git clone git://github.com/coq-ext-lib/coq-ext-lib.git
