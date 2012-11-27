5a695bdd9d8f85c8d4b23e69664d3411ff31db20
Daniel Huang <dehuang@fas.harvard.edu>
2012-11-27 18:24:19 -0500

    Finishing up CpsKSemantics.

--- src/CpsKSemantics.v
+++ src/CpsKSemantics.v
@@ -10,7 +10,7 @@ Require Import ExtLib.Core.RelDec.
 Require Import ExtLib.Structures.Reducible.
 Require Import ExtLib.Programming.Show.
 Require Import ZArith.
-
+Require Import ExtLib.Data.Strings.
 
 Set Implicit Arguments.
 Set Strict Implicit.
@@ -33,10 +33,22 @@ Section CPSEVAL.
   | Ptr_v : nat -> value
   | World_v : value.
   
+  Definition val2str (v:value) : string :=
+    match v with
+      | Con_v c => "Con(" ++ c ++ ")" 
+      | Int_v z => match z with
+                     | Z0 => "0"
+                     | Zpos p => nat2string10 (Pos.to_nat p)
+                     | Zneg p => "-" ++ nat2string10 (Pos.to_nat p)
+                   end
+      | Ptr_v n => "Ptr(" ++ nat2string10 n ++ ")"
+      | World_v => "World" 
+    end.
+
   (** Allocated values *)
   Inductive heap_value : Type := 
   | Tuple_v : list value -> heap_value
-  | Closure_v : alist var value -> list var -> exp -> heap_value.
+  | Closure_v : alist (var + cont) value -> list cont -> list var -> exp -> heap_value.
   
   (** We'll just naively implement heaps as lists of heap values *)
   Definition heap := list heap_value.
@@ -46,26 +58,27 @@ Section CPSEVAL.
   (** We'll be working in a combination of the state and option monads, so
      I've written various environment and other functions tailored to that
      monad. *)
-  Fixpoint lookup {A} (env:alist var A) (x:var) : m A := 
-    match env with 
-      | nil => raise ("unknown variable " ++ to_string x)
-      | (y,v)::rest => if eq_dec y x then ret v else lookup rest x 
+  Definition lookup {A} (env:alist (var + cont) A) (x:var + cont) : m A := 
+    match Maps.lookup x env with
+      | None => raise ("unknown variable " ++ to_string x)
+      | Some v => ret v
     end.
 
-  Definition extend {A} (env:alist var A) (x:var) (v:A) : alist var A := (x,v)::env.
+  Definition extend {A} (env:alist (var + cont) A) (x:var + cont) (v:A) : alist (var + cont) A := 
+    Maps.add x v env.
 
-  Fixpoint extends {A}(env:alist var A) (xs:list var) (vs:list A) : m (alist var A) := 
+  Fixpoint extends {A}(env:alist (var + cont) A) (xs:list (var + cont)) (vs:list A) : m (alist (var + cont) A) := 
     match xs, vs with 
       | nil, nil => ret env
       | x::xs, v::vs => extends (extend env x v) xs vs
       | _, _ => raise "calling function with wrong # of arguments"
     end.
 
-  Definition eval_op (env:alist var value) (v:op) : m value := 
+  Definition eval_op (env:alist (var + cont) value) (v:op) : m value := 
     match v with 
       | Con_o c => ret (Con_v c)
       | Int_o i => ret (Int_v i)
-      | Var_o x => lookup env x
+      | Var_o x => lookup env (inl x)
       | InitWorld_o => ret World_v
     end.
 
@@ -112,7 +125,7 @@ Section CPSEVAL.
     end.
 
   (** Evaluate the primops given closed values *)      
-  Definition eval_primop (p:primop) (vs: list value) : m value := 
+  Definition eval_primop (p:primop) (vs: list value) : m value :=
     match p, vs with 
       | Eq_p, (Int_v i)::(Int_v j)::nil => ret (if eq_dec i j then (Con_v "true") else (Con_v "false"))
       | Eq_p, (Con_v i)::(Con_v j)::nil => ret (if eq_dec i j then (Con_v "true") else (Con_v "false"))
@@ -141,17 +154,20 @@ Section CPSEVAL.
             end
           | _ => raise "projection from non-tuple"
         end
-      | _, _ => raise "bad primitive application" 
+      | _, _ => 
+        let pop := to_string p in
+        let args := fold_left (fun acc v => acc ++ val2str v ++ ", ") vs "" in
+        raise ("bad primitive application of: " ++ pop ++ " to " ++ args)%string
     end.
 
     (** Used for initializing variables to some pointer value to a dummy
        heap value in a circular set of definitions. *)
-  Definition malloc_dummy (d:decl) : m (var * value) := 
+  Definition malloc_dummy (d:decl) : m ((var + cont) * value) := 
     match d with 
-      | Fn_d f ks _ _ => v <- malloc (Tuple_v nil) ;; ret (f,v)
+      | Fn_d f ks _ _ => v <- malloc (Tuple_v nil) ;; ret (inl f,v)
       | Prim_d x MkTuple_p _ => 
         v <- malloc (Tuple_v nil) ;; 
-        ret (x,v)
+        ret (inl x,v)
       | _ => raise "can not allocate non function or tuple"
     end.
 
@@ -177,17 +193,17 @@ Section CPSEVAL.
         Another advantage of making this all explicit is that it's quite
         close to what we'll do at the machine level.
      *)
-  Definition initialize_decl (env:alist var value) (d: decl) : m unit :=
+  Definition initialize_decl (env:alist (var + cont) value) (d: decl) : m unit :=
     match d with 
       | Fn_d f ks xs e => 
-        v <- lookup env f ;;
+        v <- lookup env (inl f) ;;
         match v with 
-          | Ptr_v n => heap_set n (Closure_v env xs e)
+          | Ptr_v n => heap_set n (Closure_v env ks xs e)
           | _ => raise "??"
         end
       | Prim_d x MkTuple_p vs => 
         vs' <- mapM (eval_op env) vs ;;
-        v <- lookup env x ; 
+        v <- lookup env (inl x) ; 
         match v with 
           | Ptr_v n => heap_set n (Tuple_v vs')
           | _ => raise "??"
@@ -199,45 +215,59 @@ Section CPSEVAL.
     modify (T := list (mop * list value)) (fun x => List.app x ((mo, ls) :: nil)) ;;
     ret tt.
 
-  Fixpoint eval_decl (env:alist var value) (d:decl) : m (alist var value) := 
+  Fixpoint eval_decl (env:alist (var + cont) value) (d:decl) : m (alist (var + cont) value) := 
     match d with 
       | Op_d x v =>
         v' <- eval_op env v ;;
-        ret (extend env x v')
+        ret (extend env (inl x) v')
       | Fn_d f ks xs e =>
-        p <- malloc (Closure_v env xs e) ;;
-        ret (extend env f p)
+        p <- malloc (Closure_v env ks xs e) ;;
+        ret (extend env (inl f) p)
       | Prim_d x p vs => 
         vs' <- mapM (eval_op env) vs ;;
         v <- eval_primop p vs' ;;
-        ret (extend env x v)
+        ret (extend env (inl x) v)
       | Bind_d x w m vs => 
         vs' <- mapM (eval_op env) vs ;;
         sideEffect m vs' ;;
         ret env (** TODO **)
-        
-(*
-      | Rec_d ds => 
-        env' <- mapM malloc_dummy ds ; 
-        _ <- mapM (initialize_decl (env' ++ env)) ds ; 
-        ret (env' ++ env)
-*)
     end.
 
-  Fixpoint eval_exp (n:nat)(env:alist var value) (e:exp) : m (list value) := 
+  Definition eval_cont (env:alist (var + cont) value) (k:cont) : m value :=
+    v <- lookup env (inr k) ;;
+    ret v.
+
+  Definition malloc_dummyk (k_xs_e:cont * list var * exp) : m ((var + cont) * value) := 
+    let '(k, _, _) := k_xs_e in
+    v <- malloc (Tuple_v nil) ;;
+    ret (inr k, v).
+
+  Definition initialize_declk (env:alist (var + cont) value) (k_xs_e:cont * list var * exp) : m unit :=
+    let '(k, xs, e) := k_xs_e in
+    match Maps.lookup (inr k) env with
+      | Some (Ptr_v n) => heap_set n (Closure_v env nil xs e)
+      | _ => raise "??"
+    end.
+
+  Fixpoint eval_exp (n:nat) (env:alist (var + cont) value) (e:exp) : m (list value) :=
     match n with 
       | 0 => raise "out of gas"
       | S n => 
         match e with 
-          | App_e v ks vs => 
+          | App_e v ks os => 
             v' <- eval_op env v ;;
-            vs' <- mapM (eval_op env) vs ;;
+            vs' <- mapM (eval_op env) os ;;
+            vk' <- mapM (eval_cont env) ks ;;
+            let _ : list value := vs' in
+            let _ : list value := vk' in
             match v' with 
               | Ptr_v z => 
                 hv <- heap_lookup z ;;
                 match hv with 
-                  | Closure_v env xs e =>
-                    env' <- extends env xs vs' ;; eval_exp n env' e
+                  | Closure_v env kvs xs e =>
+                    env' <- extends env (map (fun x => inl x) xs) vs' ;;
+                    env' <- extends env' (map (fun x => inr x) kvs) vk' ;; 
+                    eval_exp n env' e
                   | _ => raise "applying non-function"
                 end
               | _ => raise "applying non-function" 
@@ -245,15 +275,37 @@ Section CPSEVAL.
           | Let_e d e =>
             env' <- eval_decl env d ;;
             eval_exp n env' e
-          | Letrec_e ds e => raise "TODO"
+          | Letrec_e ds e =>
+            env' <- mapM malloc_dummy ds ;;
+            let _ : alist (var + cont) value := env' in
+            iterM (initialize_decl (env' ++ env)%list) ds ;;
+            eval_exp n (env' ++ env)%list e
           | Switch_e v arms def => 
             v' <- eval_op env v ;;
             e <- find_arm v' arms def ;;
             eval_exp n env e
           | Halt_e o w => x <- eval_op env o ;; ret (x::nil)
-          | AppK_e _ _ => raise "TODO"
-          | LetK_e _ _ => raise "TODO"
-        end 
+          | AppK_e k os =>
+            vs <- mapM (eval_op env) os ;;
+            v <- eval_cont env k ;;
+            match v with
+              | Ptr_v z =>
+                hv <- heap_lookup z ;;
+                match hv with
+                  | Closure_v env ks xs e => 
+                    kvs <- mapM (eval_cont env) ks ;;
+                    env' <- extends env (map (fun x => inl x) xs) vs ;;
+                    env' <- extends env' (map (fun x => inr x) ks) kvs ;;
+                    eval_exp n env' e
+                  | _ => raise "applying non-function (continuation case)"
+                end
+              | _ => raise "applying non-function (continuation case)"
+            end
+          | LetK_e k_xs_e e =>
+            env' <- mapM malloc_dummyk k_xs_e ;;
+            iterM (initialize_declk (env' ++ env)%list) k_xs_e ;;
+            eval_exp n (env' ++ env)%list e
+       end 
     end.
 
   Definition run {A} (c : m A) (h:heap) : string + (A * heap * list (mop * list value)) :=
@@ -262,38 +314,41 @@ Section CPSEVAL.
   Definition eval (n:nat) (e:exp) : string + (list value * heap * list (mop * list value)) := 
     run (eval_exp n nil e) nil.
 
-(*    
+  (*
     Section TEST_EVAL.
       Import LambdaNotation. 
       Require Import ExtLib.Programming.Show.
+      Require Import CoqCompile.CpsKConvert.
       Local Open Scope string_scope.
 
-      Eval compute in to_string (CPS (gen ((\x => x) @ Z_c))).
-      Eval compute in eval 100 (CPS (gen ((\x => x) @ Z_c))).
+      Eval vm_compute in to_string (CPS_pure (gen ((\x => x) @ Z_c))).
+      Eval vm_compute in eval 100 (CPS_pure (gen ((\x => x) @ Z_c))).
 
-      Eval compute in exp2string (CPS (gen e1)).
-      Eval compute in eval 100 (CPS (gen e1)).
+      Eval vm_compute in to_string (CPS_pure (gen e1)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e1)).
 
-      Eval compute in exp2string (CPS (gen e2)).
-      Eval compute in eval 100 (CPS (gen e2)).
+      Eval vm_compute in to_string (CPS_pure (gen e2)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e2)).
 
-      Eval compute in exp2string (CPS (gen e3)).
-      Eval compute in eval 100 (CPS (gen e3)).
+      Eval vm_compute in to_string (CPS_pure (gen e3)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e3)).
 
-      Eval compute in exp2string (CPS (gen e4)).
-      Eval compute in eval 100 (CPS (gen e4)).  (** Evalutes to None in 100 steps because it diverges *)
+      Eval vm_compute in to_string (CPS_pure (gen e4)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e4)).  (** Evalutes to None in 100 steps because it diverges *)
 
-      Eval compute in exp2string (CPS (gen e5)).
-      Eval compute in eval 100 (CPS (gen e5)).  
-      
-      Eval compute in exp2string (CPS (gen e6)).
-      Eval compute in eval 100 (CPS (gen e6)).
+      Eval vm_compute in to_string (CPS_pure (gen e5)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e5)).  
 
-      Eval compute in exp2string (CPS (gen e7)).
-      Eval compute in eval 100 (CPS (gen e7)).
+      Eval vm_compute in to_string (CPS_pure (gen e6)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e6)).
 
-      Eval compute in exp2string (CPS (gen e8)).
-      Eval compute in eval 100 (CPS (gen e8)).
+      Eval vm_compute in to_string (CPS_pure (gen e7)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e7)).
+
+      Eval vm_compute in to_string (CPS_pure (gen e8)).
+      Eval vm_compute in eval 100 (CPS_pure (gen e8)).
+
     End TEST_EVAL.
-    *)
+  *)
+
 End CPSEVAL.
