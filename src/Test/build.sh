95f572494cb543e81476c1df5de68901eb2962be
Scott Moore <scott@thinkmoore.net>
2012-11-28 11:33:20 -0500

    Try to assemble during test to verify the LLVM code

--- src/Test/build.sh
+++ src/Test/build.sh
@@ -2,9 +2,12 @@
 
 coqc Ident.v
 ../Extraction/Wrapper.byte -i Ident -t ident -o ident.ll
+llvm-as ident.ll
 
 coqc Fact.v
 ../Extraction/Wrapper.byte -i Fact -t fact -o fact.ll
+llvm-as fact.ll
 
 coqc Even.v
 ../Extraction/Wrapper.byte -i Even -t even -o even.ll
+llvm-as even.ll
