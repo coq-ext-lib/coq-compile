bf9dd2bda98b8d69d45fd21b07aa98a9a0ef4953
gregory malecha <gmalecha@cs.harvard.edu>
2012-11-30 17:46:35 -0500

    finished defining domain for 0-cfa
    Conflicts:
    
    	src/Analyze/AbstractInterpCpsK.v

--- src/Analyze/AbstractInterpCpsK.v
+++ src/Analyze/AbstractInterpCpsK.v
@@ -1,4 +1,4 @@
-Require Import String.
+Require Import String List Bool.
 Require Import CoqCompile.CpsK.
 Require Import ExtLib.Structures.Maps.
 Require Import ExtLib.Structures.Monads.
@@ -6,7 +6,10 @@ Require Import ExtLib.Structures.Reducible.
 Require Import ExtLib.Data.Monads.ReaderMonad.
 Require Import ExtLib.Data.Monads.StateMonad.
 Require Import ExtLib.Data.Option.
+Require Import ExtLib.Data.Lists.
+Require Import ExtLib.Core.RelDec.
 Require Import ExtLib.Programming.Show.
+Require Import ExtLib.Data.Map.FMapAList.
 
 Import CpsK.CPSK.
 
@@ -25,7 +28,6 @@ Section AbstractDomain.
   { lookup  : Context -> ProgramPoint -> Domain -> Value
   ; update  : Context -> ProgramPoint -> Value -> Domain -> Domain
   ; joinA   : Value -> Value -> Value
-  ; uninitA : Value (** this mean uninitialized **)
   ; bottomA : Value (** this means empty, i.e. never has a value **)
   ; topA    : Value (** this means anything of any type **)
   }.
@@ -48,7 +50,7 @@ Section AbstractDomain.
 
   Class FnValue (V C D : Type) (exp : Type) : Type :=
   { injFn  : C -> list cont -> list var -> exp -> V 
-  ; applyA : forall {m} {_ : Monad m}, (C -> D -> exp -> m D%type) -> V -> list cont -> list V -> m D%type
+  ; applyA : forall {m} {_ : Monad m}, (C -> D -> exp -> m D%type) -> D -> V -> list V -> list V -> m D%type
   }.
 
   Class TplValue (V : Type) : Type :=
@@ -58,7 +60,163 @@ Section AbstractDomain.
 
 End AbstractDomain.
 
+Module CFA0.
 
+  
+  Section Context_aware.
+    Variable Context : Type.
+
+    Definition AbstractLocation : Type := var.
+    Inductive PtValue : Type :=
+    | Int : BinNums.Z -> PtValue
+(*    | Ptr : AbstractLocation -> PtValue *)
+    | Tpl : list Value -> PtValue
+    | Clo : Context -> list cont -> list var -> exp -> PtValue
+    with Value := 
+    | Values : list PtValue -> Value (** use set **)
+    | Any    : Value. 
+    Definition Domain : Type := alist (var + cont) Value.
+    Definition ProgramPoint : Type := (var + cont)%type.
+
+    Definition bottomValue : Value := Values nil.
+
+    Definition joinValue (v1 v2 : Value) : Value :=
+      match v1 , v2 with
+        | Any , _ => Any
+        | _ , Any => Any
+        | Values x , Values y => Values (x ++ y)
+      end.
+
+    Global Instance AbsDomain_CFA0 : AbsDomain Domain Value Context ProgramPoint :=
+    { lookup  := fun c p d => 
+      match Maps.lookup p d with
+        | None => bottomValue
+        | Some v => v
+      end
+    ; update  := fun c p v d =>
+      match Maps.lookup p d with
+        | None => Maps.add p v d
+        | Some v_old => Maps.add p (joinValue v_old v) d
+      end
+    ; joinA   := joinValue
+    ; bottomA := bottomValue
+    ; topA    := Any
+    }.
+
+    Global Instance IntValue_Value : IntValue Value :=
+    { injInt := fun v => 
+      match v with 
+        | None => Any
+        | Some v => Values (Int v :: nil)
+      end
+    ; plusA  := fun x y => Any
+    ; minusA := fun x y => Any
+    ; timesA := fun x y => Any
+    ; eqA    := fun _ _ => None
+    ; ltA    := fun _ _ => None
+    }.
+
+    Global Instance BoolValue_Value : BoolValue Value :=
+    { injBool := fun x => Any
+    ; may := fun _ _ => true
+    ; must := fun _ _ => false
+    ; orA := fun _ _ => Any      
+    }.
+
+    Fixpoint list_join_lenAny (ls ls' : list Value) : list Value :=
+      match ls , ls' with
+        | nil , ls' => ls'
+        | ls , nil => ls
+        | l :: ls , l' :: ls' =>
+          joinA l l' :: list_join_lenAny ls ls'
+      end.
+
+    Fixpoint filter_map {A B} (f : A -> option B) (ls : list A) : list B :=
+      match ls with
+        | nil => nil
+        | l :: ls => 
+          match f l with
+            | None => filter_map f ls
+            | Some v => v :: filter_map f ls
+          end
+      end.
+
+    Definition getNats (ls : list PtValue) : list nat :=
+      filter_map (fun x =>
+        match x with
+          | Int BinNums.Z0 => Some 0
+          | Int (BinNums.Zpos n) => Some (BinPos.Pos.to_nat n)
+          | _ => None
+        end) ls.
+
+    Global Instance TplValue_Value : TplValue Value :=
+    { injTuple := fun ls => Values (Tpl ls :: nil)
+    ; projA    := fun n t =>
+      match t with
+        | Any => Any
+        | Values vs =>
+          let ptwise_tuple := List.fold_left (fun acc x => 
+            match x with
+              | Tpl vs => list_join_lenAny vs acc
+              | _ => acc
+            end) vs nil in
+          match n with
+            | Any => List.fold_left joinA ptwise_tuple bottomA 
+            | Values v => 
+              let nats := getNats v in
+              let all := List.map (fun n => 
+                match nth_error ptwise_tuple n with
+                  | None => bottomA
+                  | Some x => x
+                end) nats in
+              List.fold_left joinA all bottomA
+          end
+      end
+    }.
+
+    Definition getClos (ls : list PtValue) : list (Context * list cont * list var * exp) :=
+      filter_map (fun x =>
+        match x with
+          | Clo c xs vs e => Some (c, xs, vs, e)
+          | _ => None
+        end) ls.
+
+    Global Instance FnValue_Value : FnValue Value Context Domain exp :=
+    { injFn := fun c ks xs e => Values (Clo c ks xs e :: nil)
+    ; applyA := fun _ _ aeval dom v ks vs' =>
+      match v with
+        | Any => ret dom (** TODO : think more about this! **)
+        | Values vs =>
+          let clos := getClos vs in
+          foldM (fun x acc => let '(c, ks', xs', e) := x in
+            if eq_dec (List.length ks') (List.length ks) &&
+               eq_dec (List.length xs') (List.length vs) then
+              let ext := List.combine (List.map (fun x => inr x) ks') ks 
+                      ++ List.combine (List.map (fun x => inl x) xs') vs' in
+              let dom' := fold_left (fun acc x => update c (fst x) (snd x) acc) ext dom in 
+              aeval c dom' e
+            else
+              ret acc) (ret dom) clos
+      end
+    }.
+(*
+    refine (
+      aks axs = values from memo table ;;
+      if ks <= aks && vs' <= axs then 
+        ret dom
+      else
+        aeval c dom' e
+    ).
+*)
+
+  End Context_aware.
+
+End CFA0.
+
+
+(** NOTE: This is poorly coded
+ ** Don't use a random second monad to be cool
+ **)
 Section AI.
   Variables D C V : Type.
   Context {AbsTime_C  : AbsTime C}.
@@ -71,19 +229,14 @@ Section AI.
   Import MonadNotation.
   Local Open Scope monad_scope.
   Local Open Scope list_scope.
-  
-
-(*
-  Section Maps.
-    Variable map_var : Type -> Type.
-    Context {FM : DMap Env.var map_var}.
-*)
-    
+     
     Section Monadic.
       Variable m : Type -> Type.
       Context {Monad_m : Monad m}.
       Context {Exc_m : MonadExc string m}.
       Context {Fix_m : MonadFix m}.
+      (** BFS **)
+      Context {State_m : MonadState (alist (C * exp) D) m}.
 
       Definition m' : Type -> Type := 
         readerT C (stateT D m).
@@ -102,18 +255,7 @@ Section AI.
           | InitWorld_o => ret bottomA (** TODO: ?? **)
         end.
 
-(**
-      Definition insert (v : var) (a : V) : m V :=
-        ctx    <- ask ;;
-        domain <- get ;;
-        let new := joinA (lookup ctx v domain) a in
-        put (add v new mp) ;;
-        ret new.
-**)
-
       Section Transfer.
-        Parameter admit : forall {A}, A.
-
         Definition illFormed_decl {A} (d : decl) : m A :=
           raise ("Ill-formed declaration " ++ runShow (show d))%string.
         
@@ -169,15 +311,29 @@ Section AI.
           modify (update ctx (inl v) vA) ;;
           ret tt.
 
+        Definition init_decl (d : decl) : m' unit :=
+          ctx <- ask (T := C) ;;
+          match d return m' _ with
+            | Op_d v _ 
+            | Prim_d v _ _
+            | Fn_d v _ _ _ => 
+              modify (update ctx (inl v) bottomA)
+            | Bind_d v1 v2 _ _ =>
+              modify (update ctx (inl v1) bottomA) ;;
+              modify (update ctx (inl v2) bottomA)
+          end ;;
+          ret tt.
+
         Definition aeval : C -> D -> exp -> m D :=
           mfix3 _ (fun aeval => fix recur (ctx : C) (dom : D) (e : exp) : m D :=
             match e return m D with
               | App_e o ks os =>
+                let argsK := List.map (fun k => lookup ctx (inr k) dom) ks in
                 fA_argsA_d <- runM' (fA <- eval_op o ;;
                                      argsA <- mapM eval_op os ;;
-                                     ret (fA, argsA)) ctx dom ;;
-                let '((fA, argsA), dom) := fA_argsA_d in
-                applyA aeval fA ks argsA 
+                                     ret (fA, argsK, argsA)) ctx dom ;;
+                let '((fA, argsK, argsA), dom) := fA_argsA_d in
+                applyA aeval dom fA argsK argsA 
               | Let_e d e =>
                 v_dom <- runM' (eval_decl d) ctx dom ;;
                 let '(v, dom) := v_dom in
@@ -188,11 +344,17 @@ Section AI.
                                      ret (fA, argsA)) ctx dom ;;
                 let _ : ((V * list V) * D) := fA_argsA_d in
                 let '((fA, argsA), dom)  := fA_argsA_d in
-                applyA aeval fA nil argsA 
-              | LetK_e ks e => admit
+                applyA aeval dom fA nil argsA 
+              | LetK_e ks e =>
+                dom' <- runM' 
+                  (iterM (fun kxse => let '(k,xs,e) := kxse in
+                    modify (update ctx (inr k) (injFn ctx nil xs e)) ;;
+                    ret tt) ks) ctx dom ;;
+                recur ctx (snd dom') e
               | Letrec_e ds e =>
-                (** TODO: we aren't tying the knot correctly **)
-                dom' <- runM' (iterM eval_decl ds) ctx dom ;;
+                dom' <- runM' 
+                  (iterM init_decl ds ;;
+                   iterM eval_decl ds) ctx dom ;;
                 recur ctx (snd dom') e
               | Switch_e o arms e =>
                 s <- runM' (eval_op o) ctx dom ;;
@@ -210,35 +372,6 @@ Section AI.
 
       End Transfer.
 
-(*
-      Definition D := ((list decl) * bool)%type. (* fns, escapes *)
-      Definition bottomD : D := (nil,false).
-      Definition joinD (a b : D) : D :=
-        match a , b with
-          | (afs,ab),(bfs,bb) => (afs++bfs,orb ab bb)
-        end.
-
-      (* structure mostly right...*)
-      Definition contApplyA (aeval : exp -> m D) (fn : D) (args : list D) : m D :=
-        match fn with
-          | (fns,b) =>
-            results <- mapM (fun (d:decl) =>
-              match d with
-                | Fn_d v formals e => 
-                  mapM (fun v => match v with
-                                   | (f,a) => insert f a
-                                 end) (List.combine formals args) ;;
-                  aeval e
-                | _ => ret bottomD
-              end) fns ;;
-            ret (fold (fun a b => ret (joinD a b)) (ret bottomD) results)
-        end.
-        
-
-        Parameter primA : (exp -> m A) -> primop -> list A -> m A.
-        Parameter fnA : (exp -> m A) -> decl -> m A.
-        Parameter bindA : (exp -> m A) -> mop -> list A -> m (A * A).
-*)
     End Monadic.
 End AI.
 
