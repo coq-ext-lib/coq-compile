6dc881af01ae14ee9aac09ec06fa24308e7b8f43
gregory malecha <gmalecha@cs.harvard.edu>
2013-01-10 17:16:59 -0500

    a nicer encoding of rewrite rules.

--- src/coq/Opt/Rewrite.v
+++ src/coq/Opt/Rewrite.v
@@ -1,35 +1,27 @@
 Require Import ExtLib.Data.HList.
+Require Import List.
 
 Set Implicit Arguments.
 Set Strict Implicit.
 
-Inductive mem (T : Type) : list T -> T -> Type :=
-| memH : forall t ls, mem (t :: ls) t
-| memS : forall t t' ls, mem ls t -> mem (t' :: ls) t.
-
-Arguments memH {_ _ _}.
-Arguments memS {_ _ _ _} _.
-
-
 Class OptEquation (T : Type) : Type := OptRewrite
 { env   : list Type
-; left  : (forall {t}, mem env t -> t) -> T
-; right : (forall {t}, mem env t -> t) -> T
+; left  : (forall n, nth n env unit) -> T
+; right : (forall n, nth n env unit) -> T
 ; proof : forall g, left g = right g
 }.
 
 (** Example rewrite **)
-Require Import List.
-
-Notation "g # 1" := (g _ memH) (at level 0).
-Notation "g # 2" := (g _ (memS memH)) (at level 0).
-Notation "g # 3" := (g _ (memS (memS memH))) (at level 0).
-
 Instance Eqn_hd (T : Type) : OptEquation T :=
 { env   := T :: list T :: T :: nil 
-; left  := fun g => hd (g#3) (cons (g#1) (g#2))
-; right := fun g => g T memH
+; left  := fun g => hd (g 2) (cons (g 0) (g 1))
+; right := fun g => g 0
 ; proof := _
 }.
 reflexivity.
-Qed.
+Defined.
+
+(**
+Extraction Language Scheme.
+Recursive Extraction Eqn_hd.
+**)
