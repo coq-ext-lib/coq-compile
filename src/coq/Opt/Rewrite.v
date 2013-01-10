222be6ab611ddfe530e3d9c8867acb9fc59e9a53
gregory malecha <gmalecha@cs.harvard.edu>
2013-01-10 17:24:31 -0500

    tiny bit of code cleanup.

--- src/coq/Opt/Rewrite.v
+++ src/coq/Opt/Rewrite.v
@@ -15,7 +15,14 @@ Instance Eqn_hd (T : Type) : OptEquation T :=
 { env   := T :: list T :: T :: nil 
 ; left  := fun g => hd (g 2) (cons (g 0) (g 1))
 ; right := fun g => g 0
-; proof := _
+}.
+reflexivity.
+Defined.
+
+Instance Eqn_uncurry (A B : Type) (T : Type) : OptEquation T :=
+{ env   := (A -> B -> T) :: A :: B :: nil
+; left  := fun g => (g 0) (g 1) (g 2)
+; right := fun g => (fun x => (g 0) (fst x) (snd x)) (g 1, g 2)
 }.
 reflexivity.
 Defined.
